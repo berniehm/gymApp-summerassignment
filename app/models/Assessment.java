3b9eb98211caa80026756c669de132a528d5b27d
Eamonn de Leastar <edeleastar@wit.ie>
2017-03-20 10:19:09 +0000

    assessment model + bootstrap + initial data

---
+++ app/models/Assessment.java
@@ -0,0 +1,26 @@
+package models;
+
+import play.db.jpa.Model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Assessment extends Model
+{
+  public double weight;
+  public double chest;
+  public double thigh;
+  public double upperarm;
+  public double waist;
+  public double hips;
+
+  public Assessment(double weight, double chest, double thigh, double upperarm, double waist, double hips)
+  {
+    this.weight = weight;
+    this.chest = chest;
+    this.thigh = thigh;
+    this.upperarm = upperarm;
+    this.waist = waist;
+    this.hips = hips;
+  }
+}
