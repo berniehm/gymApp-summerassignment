9f627d815c600bf99e88c95c17c4a75f64ecffbc
Eamonn de Leastar <edeleastar@wit.ie>
2017-03-22 09:04:06 +0000

    tidy up - bootstrap + gitignore

--- app/Bootstrap.java
+++ app/Bootstrap.java
@@ -1,12 +1,19 @@
-import play.jobs.Job;
-import play.jobs.OnApplicationStart;
-import play.test.Fixtures;
+import java.util.List;
+
+import play.*;
+import play.jobs.*;
+import play.test.*;
+
+import models.*;
 
 @OnApplicationStart
 public class Bootstrap extends Job
 {
   public void doJob()
   {
-    Fixtures.loadModels("data.yml");
+    if (Member.count() == 0)
+    {
+      Fixtures.loadModels("data.yml");
+    }
   }
 }
